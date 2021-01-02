a4c035c6a9b2622e98b5afe83b955bedd12557bd
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-03 00:59:20 +0900

    Componentをmoduleに纏める。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material/material.module';
+import { ComponentModule } from './component/component.module';
+
+import { AppComponent } from './app.component';
 import { ComponentListComponent } from './page/component-list/component-list.component';
-import { RowComponent } from './component/row/row.component';
-import { ColumnComponent } from './component/column/column.component';
-import { InputTextComponent } from './component/input-text/input-text.component';
-import { ButtonComponent } from './component/button/button.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ComponentListComponent,
-    RowComponent,
-    ColumnComponent,
-    InputTextComponent,
-    ButtonComponent,
-  ],
+  declarations: [AppComponent, ComponentListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    BrowserAnimationsModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MaterialModule,
+    ComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/component/component.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialModule } from '../material/material.module';
+
+import { RowComponent } from './row/row.component';
+import { ColumnComponent } from './column/column.component';
+import { InputTextComponent } from './input-text/input-text.component';
+import { ButtonComponent } from './button/button.component';
+
+@NgModule({
+  declarations: [
+    RowComponent,
+    ColumnComponent,
+    InputTextComponent,
+    ButtonComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+  ],
+  exports: [RowComponent, ColumnComponent, InputTextComponent, ButtonComponent],
+})
+export class ComponentModule {}
