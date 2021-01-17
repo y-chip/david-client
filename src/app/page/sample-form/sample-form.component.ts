a952958b487bce5561ff060eab3f2cdb3ad59f2b
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-17 15:33:54 +0900

    クライアントチェックでエラーがあったときは、送信しないようにする。

--- src/app/page/sample-form/sample-form.component.ts
+++ src/app/page/sample-form/sample-form.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { SampleFormService } from '../../service/sample-form/sample-form.service';
 import { Sample } from '@david/david-api';
 import { SnackbarService } from '../../service/snackbar/snackbar.service';
 import { ProgressSpinnerOverlayService } from '../../service/progress-spinner-overlay/progress-spinner-overlay.service';
+import { FieldComponent } from '../../component/field/field.component';
+import { BannerService } from '../../service/banner/banner.service';
 
 @Component({
   selector: 'app-sample-form',
@@ -10,13 +12,16 @@ import { ProgressSpinnerOverlayService } from '../../service/progress-spinner-ov
   styleUrls: ['./sample-form.component.css'],
 })
 export class SampleFormComponent implements OnInit {
+  @ViewChildren(FieldComponent) fields: QueryList<FieldComponent<any>> | null;
   sample: Sample;
 
   constructor(
     private sampleFormService: SampleFormService,
+    private bannerService: BannerService,
     private snackbarService: SnackbarService,
     private progressSpinnerOverlayService: ProgressSpinnerOverlayService
   ) {
+    this.fields = null;
     this.sample = { id: null, text: null, longText: null, number: null };
   }
 
@@ -30,11 +35,26 @@ export class SampleFormComponent implements OnInit {
   }
 
   save(): void {
+    if (this.invalid()) {
+      this.bannerService.open('入力エラーがあります。', 'error');
+      return;
+    }
+
     this.sampleFormService.post(this.sample).subscribe((sample) => {
       this.sample = sample;
       this.progressSpinnerOverlayService.close();
       this.snackbarService.open('保存しました。');
       return;
+    });
+  }
+
+  invalid(): boolean {
+    if (this.fields == null) {
+      return false;
+    }
+
+    return this.fields.some((f) => {
+      return !!f.input?.invalid;
     });
   }
 }
