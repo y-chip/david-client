07e3bcd606811eb4b66800bbad8d13d025f01fe1
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-18 23:16:04 +0900

    PageComponentを継承する。

--- src/app/page/component-list/component-list.component.ts
+++ src/app/page/component-list/component-list.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { PageComponent } from '../page.component';
 
 @Component({
   selector: 'app-component-list',
   templateUrl: './component-list.component.html',
   styleUrls: ['./component-list.component.css'],
 })
-export class ComponentListComponent implements OnInit {
-  constructor() {}
+export class ComponentListComponent extends PageComponent implements OnInit {
+  constructor(injector: Injector) {
+    super(injector);
+  }
 
   ngOnInit(): void {}
 }
