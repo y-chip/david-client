6e432442e592751f91126516b5853963156a3588
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-30 20:30:05 +0900

    sidenavを開くとき、常にoverにする。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnDestroy {
-  private queryList: MediaQueryList;
-  private readonly onChangeQueryList: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.queryList = media.matchMedia('(max-width: 600px)');
-    this.onChangeQueryList = () => changeDetectorRef.detectChanges();
-    this.queryList.addEventListener('change', this.onChangeQueryList);
-  }
-
-  ngOnDestroy(): void {
-    this.queryList.removeEventListener('change', this.onChangeQueryList);
-  }
-
-  isSmallDisplay(): boolean {
-    return this.queryList.matches;
-  }
+export class AppComponent {
+  constructor() {}
 }
